86fd27e252f2fb10765fd38236106dad30e76cf8
surendratamang <tamangsurendra44@gmail.com>
2019-07-21 22:11:53 +0545

    do it

---
+++ script.js
@@ -0,0 +1,90 @@
+let audio = new Audio('alarm-1.ogg');
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const deadLineDisplay = document.querySelector('#exam_day');
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    // this is for count down copied from w3schools
+    
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it!
+    if(secondsLeft < 0) {
+      audio.play();
+      clearInterval(countdown);
+      return;
+    }
+  
+ 
+    // display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
+
+setInterval(() => {
+
+  // Get today's date and time
+  var now1 = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now1;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+ 
+  // If the count down is finished, write some text 
+  
+  // Display the result in the element with id="demo"
+  let display1 = `${days}:${hours}:${minutes}:${seconds < 10 ? '0' : '' }${seconds}`;
+  
+  deadLineDisplay.textContent = display1;
+
+  // If the count down is finished, write some text 
+  
+}, 1000)
